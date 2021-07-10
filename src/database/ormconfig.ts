0ee11614c26369a7f949e2775a5d1935db3e27a0
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-07-10 00:54:30 -0500

    Update ormconfig.ts

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -4,7 +4,7 @@ import { ConnectionOptions } from 'typeorm'
 import { PROD_ENV } from '../constants'
 
 const config = {
-  host: 'localhost',
+  host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_DATABASE,
@@ -20,7 +20,7 @@ if (
 
 const connectionOptions: ConnectionOptions = {
   type: 'postgres',
-  host: config.host,
+  host: config.host || 'localhost',
   port: 5432,
   username: config.user || 'postgres',
   password: config.password || 'postgres',
