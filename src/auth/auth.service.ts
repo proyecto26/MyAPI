8b2f9814f3019be90da1adca2ed0789cd6d99da4
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-08-07 19:23:51 -0500

    Fix linter issue

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
-import { get, isEmpty } from 'lodash'
+import { isEmpty } from 'lodash'
 
 import { User } from '../models/user'
 import { AuthToken, AuthPayload } from '../models/auth'
